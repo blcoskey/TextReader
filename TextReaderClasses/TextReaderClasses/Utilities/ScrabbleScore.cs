2037893724d05344fe7cf55c0e8a3cc10ce6dca8
blcoskey <blcoskey@gmail.com>
2019-04-10 12:41:25 +0200

    Improved Score performance

--- TextReaderClasses/TextReaderClasses/Utilities/ScrabbleScore.cs
+++ TextReaderClasses/TextReaderClasses/Utilities/ScrabbleScore.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,56 +7,28 @@ namespace TextReaderClasses.Utilities
 {
     public class ScrabbleScore
     {
-        //SortedDictionary to hold scrabble scores for letters
-        public SortedDictionary<char, int> LetterScores;
-
         public ScrabbleScore()
         {
-            //Add all scores into SortedDictionary
-            LetterScores = new SortedDictionary<char, int>();
-
-            LetterScores.Add('a', 1);
-            LetterScores.Add('b', 3);
-            LetterScores.Add('c', 3);
-            LetterScores.Add('d', 2);
-            LetterScores.Add('e', 1);
-            LetterScores.Add('f', 4);
-            LetterScores.Add('g', 2);
-            LetterScores.Add('h', 4);
-            LetterScores.Add('i', 1);
-            LetterScores.Add('j', 8);
-            LetterScores.Add('k', 5);
-            LetterScores.Add('l', 1);
-            LetterScores.Add('m', 3);
-            LetterScores.Add('n', 1);
-            LetterScores.Add('o', 1);
-            LetterScores.Add('p', 3);
-            LetterScores.Add('q', 10);
-            LetterScores.Add('r', 1);
-            LetterScores.Add('s', 1);
-            LetterScores.Add('t', 1);
-            LetterScores.Add('u', 4);
-            LetterScores.Add('v', 4);
-            LetterScores.Add('w', 4);
-            LetterScores.Add('x', 8);
-            LetterScores.Add('y', 4);
-            LetterScores.Add('z', 10);
         }
 
         public int CalculateScore(string word)
         {
-
+            int[] scores = { 1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 4, 4, 4, 8, 4, 10};
             var score = 0;
 
-            //Convert word to lowercase, just in "case"
-            word = word.ToLower();
-
-            //loop all char in word
-            foreach(char letter in word)
+            for (var x = 0; x < word.Length; x++)
             {
-                //get score for letter
-                score += LetterScores.Where(x => x.Key == letter).FirstOrDefault().Value;
+                var index = word[x] - 97;
+                if(index < 0 || index > 25)
+                {
+
+                }
+                else
+                {
+                    score += scores[index];
+                }
             }
+
             return score;
         }
     }
